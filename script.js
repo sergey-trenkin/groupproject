884ec308691ca6f4a88dba4010f369ebffc5da69
Sergey <140949059+sergey-trenkin@users.noreply.github.com>
2026-03-09 15:04:08 +0000

    add script

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello, World! This is a simple JavaScript file.");
+
+function greet(name) {
+    return `Hello, ${name}! Welcome to JavaScript programming.`;
+}
+
+console.log(greet("Alice"));
